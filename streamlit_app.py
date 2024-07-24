1635b666503b26dce29b6b0d8a19ec8128e3e33a
rafaelbabar <rafaelbabar@gmail.com>
2024-07-24 16:47:34 +0100

    Delete sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ def propertypulse():
     # Read the CSV file once
     df = pd.read_csv("ppd_data.csv", encoding="utf-8-sig")
     
-    search = st.sidebar.radio("Please select a criteria", 
+    search = st.radio("Please select a criteria", 
                               ("Price", "Post Code", "Property Type", "Transaction Category"))
     
     if search == "Post Code":
-        enterpostcode = st.sidebar.text_input("Please enter a post code")
-        if st.sidebar.button("Check Post Code"):
+        enterpostcode = st.text_input("Please enter a post code")
+        if st.button("Check Post Code"):
             filtered_df = df[df['postcode'] == enterpostcode]
             if not filtered_df.empty:
                 st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'linked_data_uri']])
@@ -24,11 +24,11 @@ def propertypulse():
     elif search == "Property Type":
         property_types = df['property_type'].unique().tolist()
         ptsearch = st.selectbox("Please select a property type", [""] + property_types)
-        st.sidebar.write("F - Flat")
-        st.sidebar.write("O - Other")
-        st.sidebar.write("T - Terraced")
-        st.sidebar.write("D - Detached")
-        st.sidebar.write("S - Semi Detached")
+        st.write("F - Flat")
+        st.write("O - Other")
+        st.write("T - Terraced")
+        st.write("D - Detached")
+        st.write("S - Semi Detached")
         if ptsearch:
             filtered_df = df[df['property_type'] == ptsearch]
             if not filtered_df.empty:
@@ -39,8 +39,8 @@ def propertypulse():
     elif search == "Transaction Category":
         transaction_categories = df['transaction_category'].unique().tolist()
         tcsearch = st.selectbox("Please select a transaction category", [""] + transaction_categories)
-        st.sidebar.write("B - Additional price paid transaction")
-        st.sidebar.write("A - Standard price paid transaction")
+        st.write("B - Additional price paid transaction")
+        st.write("A - Standard price paid transaction")
         if tcsearch:
             filtered_df = df[df['transaction_category'] == tcsearch]
             if not filtered_df.empty:
@@ -49,9 +49,9 @@ def propertypulse():
                 st.write("No results found for the selected transaction category.")
     
     elif search == "Price":
-        min_price = st.sidebar.number_input("Please enter a minimum price", value=0)
-        max_price = st.sidebar.number_input("Please enter a maximum price", value=1000000)
-        if st.sidebar.button("Check Price Range"):
+        min_price = st.number_input("Please enter a minimum price", value=0)
+        max_price = st.number_input("Please enter a maximum price", value=1000000)
+        if st.button("Check Price Range"):
             filtered_df = df[(df['price_paid'] >= min_price) & (df['price_paid'] <= max_price)]
             if not filtered_df.empty:
                 st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode', 'linked_data_uri']])
