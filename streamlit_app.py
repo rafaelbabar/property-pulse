55ad3fb2dbcd5992b1bf590fe1df22410a66c203
rafaelbabar <rafaelbabar@gmail.com>
2024-07-30 22:23:59 +0100

    tables separated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#D:\OneDrive\Desktop\Projects>streamlit run property-pulse-v4.py
+#D:\OneDrive\Desktop\Projects>streamlit run property-pulse-v6.py
 
 def propertypulse():
     st.title("Property Sales - Manchester")
@@ -17,34 +17,37 @@ def propertypulse():
         if st.button("Check Post Code"):
             filtered_df = df[df['postcode'] == enterpostcode]
             if not filtered_df.empty:
-                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'linked_data_uri']])
+                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street']])
+                st.dataframe(filtered_df[['linked_data_uri']])
             else:
                 st.write("No results found for the entered post code.")
     
     elif search == "Property Type":
+        property_types = df['property_type'].unique().tolist()
+        ptsearch = st.selectbox("Please select a property type", [""] + property_types)
         st.write("F - Flat")
         st.write("O - Other")
         st.write("T - Terraced")
         st.write("D - Detached")
         st.write("S - Semi Detached")
-        property_types = df['property_type'].unique().tolist()
-        ptsearch = st.selectbox("Please select a property type", [""] + property_types)
         if ptsearch:
             filtered_df = df[df['property_type'] == ptsearch]
             if not filtered_df.empty:
-                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode', 'linked_data_uri']])
+                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode']])
+                st.dataframe(filtered_df[['linked_data_uri']])
             else:
                 st.write("No results found for the selected property type.")
     
     elif search == "Transaction Category":
-        st.write("B - Additional price paid transaction")
-        st.write("A - Standard price paid transaction")
         transaction_categories = df['transaction_category'].unique().tolist()
         tcsearch = st.selectbox("Please select a transaction category", [""] + transaction_categories)
+        st.write("B - Additional price paid transaction")
+        st.write("A - Standard price paid transaction")
         if tcsearch:
             filtered_df = df[df['transaction_category'] == tcsearch]
             if not filtered_df.empty:
-                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode', 'linked_data_uri']])
+                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode']])
+                st.dataframe(filtered_df[['linked_data_uri']])
             else:
                 st.write("No results found for the selected transaction category.")
     
@@ -54,7 +57,8 @@ def propertypulse():
         if st.button("Check Price Range"):
             filtered_df = df[(df['price_paid'] >= min_price) & (df['price_paid'] <= max_price)]
             if not filtered_df.empty:
-                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode', 'linked_data_uri']])
+                st.dataframe(filtered_df[['price_paid', 'deed_date', 'saon', 'paon', 'street', 'postcode']])
+                st.dataframe(filtered_df[['linked_data_uri']])
             else:
                 st.write("No results found for the selected price range.")
 
