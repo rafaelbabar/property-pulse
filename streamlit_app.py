27626fca7f61c96e33d8ab3c029e52de8d55f5d9
rafaelbabar <rafaelbabar@gmail.com>
2024-07-31 09:44:22 +0100

    Session State for Post Code and Price

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
+#D:\OneDrive\Desktop\Projects>streamlit run property-pulse-v8.py
 import streamlit as st
 import pandas as pd
-#D:\OneDrive\Desktop\Projects>streamlit run property-pulse-v7.py
+
 def propertypulse():
     st.title("Property Sales - Manchester")
     st.subheader("1 January 2020 - last month")
@@ -19,15 +20,18 @@ def propertypulse():
         if row_selection is not None:
             selected_link = filtered_df.loc[row_selection, 'linked_data_uri']
             st.markdown(make_clickable(selected_link), unsafe_allow_html=True)
-    
+
     if search == "Post Code":
         enterpostcode = st.text_input("Please enter a post code")
+        if 'filtered_df_postcode' not in st.session_state:
+            st.session_state['filtered_df_postcode'] = pd.DataFrame()
         if st.button("Check Post Code"):
-            filtered_df = df[df['postcode'] == enterpostcode]
-            if not filtered_df.empty:
-                display_filtered_data(filtered_df)
-            else:
-                st.write("No results found for the entered post code.")
+            st.session_state['filtered_df_postcode'] = df[df['postcode'] == enterpostcode]
+        filtered_df = st.session_state['filtered_df_postcode']
+        if not filtered_df.empty:
+            display_filtered_data(filtered_df)
+        else:
+            st.write("No results found for the entered post code.")
     
     elif search == "Property Type":
         property_types = df['property_type'].unique().tolist()
@@ -59,11 +63,14 @@ def propertypulse():
     elif search == "Price":
         min_price = st.number_input("Please enter a minimum price", value=0)
         max_price = st.number_input("Please enter a maximum price", value=1000000)
+        if 'filtered_df_price' not in st.session_state:
+            st.session_state['filtered_df_price'] = pd.DataFrame()
         if st.button("Check Price Range"):
-            filtered_df = df[(df['price_paid'] >= min_price) & (df['price_paid'] <= max_price)]
-            if not filtered_df.empty:
-                display_filtered_data(filtered_df)
-            else:
-                st.write("No results found for the selected price range.")
+            st.session_state['filtered_df_price'] = df[(df['price_paid'] >= min_price) & (df['price_paid'] <= max_price)]
+        filtered_df = st.session_state['filtered_df_price']
+        if not filtered_df.empty:
+            display_filtered_data(filtered_df)
+        else:
+            st.write("No results found for the selected price range.")
 
 propertypulse()
